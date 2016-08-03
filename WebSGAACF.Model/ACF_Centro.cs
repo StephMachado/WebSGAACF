43e611cf9fd6aeab0e0535ca9e7f2ee66ebb8666
StephMachado <steph_machado@hotmail.com>
2016-08-02 23:45:36 -0500

    WebSGAACF - Proyecto de prueba

---
+++ WebSGAACF.Model/ACF_Centro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSGAACF.Model
+{
+    public partial class ACF_Centro
+    {
+        public string Id_Centro { get; set; }
+    }
+}
