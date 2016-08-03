7e7761fac2f1a7870eb6870c23bb84850ee44d6d
StephMachado <steph_machado@hotmail.com>
2016-08-03 00:05:09 -0500

    Entidades del modelo - 2016/08/03

--- WebSGAACF.Model/ACF_Centro.cs
+++ WebSGAACF.Model/ACF_Centro.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebSGAACF.Model
 {
     public partial class ACF_Centro
     {
+        [Key]
         public string Id_Centro { get; set; }
+
+        [Required()]
+        public string Nombre { get; set; }
+
+        public string UsuarioRegistro { get; set; }
+
+        public DateTime FechaRegistro { get; set; }
+
+        public bool FlagAnulado { get; set; }
+
+        public string Cod_Sociedad { get; set; }
+
+        public string Cod_Centro { get; set; }
     }
 }
