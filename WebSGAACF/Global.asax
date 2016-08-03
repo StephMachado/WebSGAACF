177b28203dcba6fddeb82c47b44ddce3ff29c31d
StephMachado <steph_machado@hotmail.com>
2016-08-02 22:23:15 -0500

    Add project files.

---
+++ WebSGAACF/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebSGAACF.MvcApplication" Language="C#" %>
---
+++ WebSGAACF/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebSGAACF.Startup))]
+namespace WebSGAACF
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
